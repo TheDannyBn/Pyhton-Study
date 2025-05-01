f9a72d83742ab8d7816ca7c37be89c9397919d4d
Danny <dani.baran3@gmail.com>
2025-05-01 11:55:28 +0300

    Python Basic

---
+++ basicWithPython/Chapter 1.py
@@ -0,0 +1,23 @@
+#Chapter 1 – Python Basics
+#Print the word "Hello".
+#Print the result of adding 5 and 3.
+#Define a variable age with the value 25 and print it.
+#Print the result of dividing 10 by 2.
+#Define a variable name with a name and print it.
+#Print the length of the word "Python".
+#Define a float variable and print it.
+#Use input() to get the user's name and print it.
+#Print the result of 1 + 2 * 3.
+#Create a variable is_happy and set it to True, then print it.
+
+
+#print('hello')
+#print(5+3)
+#age = 25 print(age)
+#print(10/2)
+#name = 'Danny' print(name)
+#print(len('Python'))
+#Pi= 3.14 print(pi)
+#name= input("Whats your name?") print(name)
+#print (1+2*3)
+#is_happy = True print(is_happy)
---
+++ basicWithPython/Chapter 2.py
@@ -0,0 +1,11 @@
+#Chapter 2 – Flow Control
+#If a number is greater than 10, print "Big".
+#If the user's age is over 18, print "Allowed".
+#Print "Congrats!" if the age is between 10 and 20.
+#If the number is even, print "Even", otherwise print "Odd".
+#If the age is negative or over 120, print "Invalid Age".
+#Create a variable weather set to "rainy", and print "Take an umbrella!" if it's rainy.
+#Print "Hello World" if the variable is_active is True.
+#Generate a random number between 1 and 10 and print "You won!" if it's 7.
+#If the user's age is over 50, print "Old".
+#If the number is greater than 100, print "Big Number".
