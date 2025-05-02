4e40d3eac82e78dad5d8dfc7d7237479672dd90a
Danny <dani.baran3@gmail.com>
2025-05-02 23:40:49 +0300

    Practice with dictionary

--- basicWithPython/Chapter 5.py
+++ basicWithPython/Chapter 5.py
@@ -10,3 +10,32 @@
 #Change a value in a dictionary.
 #Use get() to safely retrieve a key that may not exist.
 
+my_dict = {"name": "Danny", "age": 27}
+print(my_dict["name"])
+
+my_dict['city'] = "Ashdod"
+print(my_dict)
+
+del my_dict["age"]
+print(my_dict)
+
+for key in my_dict:
+    print(key)
+
+for value in my_dict.values():
+    print(value)
+
+if "name" in my_dict:
+    print("Name key exists!")
+
+people = [
+    {"name": "Danny", "age": 27,"city": "Ashdod"},
+    {"name": "Sherry", "age": 30,"city": "Tel Aviv"},
+    {"name": "Mali", "age": 25,"city": "Ashkelon"}
+]
+
+people[0]["age"] = 26
+print(people[0])
+
+job = my_dict.get("job", "Not specified")
+print(job)
